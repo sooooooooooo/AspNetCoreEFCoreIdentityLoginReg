468a101325511919d6520ecdde5142dae5bef20e
sooooooooooo <zhigengqi@yahoo.com>
2020-12-05 20:49:42 -0600

    added Registration form & functionnality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,23 +2,64 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading.Tasks; // for using: Because our User management functions are all asynchronous, we'll have to declare our Controller methods as async to make use of them.
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LoginRegWithIdentity.Models;
+using LoginRegWithIdentity.ViewModels;
+using Microsoft.AspNetCore.Identity; // dependency injection
 
 namespace LoginRegWithIdentity.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private MyContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MyContext context, UserManager<User> userManager, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
+        [HttpGet("register")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create a new User object, without adding a Password
+                User NewUser = new User { Email = viewModel.Email, UserName = viewModel.UserName };
+                // CreateAsync will attempt to create the User in the database, simultaneously hashing the password
+                IdentityResult result = await _userManager.CreateAsync(NewUser, viewModel.Password);
+                // If the User was added to the database successfully
+                if (result.Succeeded)
+                {
+                    // Sign In the newly created User
+                    // We're using the SignInManager, not the UserManager!
+                    await _signInManager.SignInAsync(NewUser, isPersistent: false);
+                    return RedirectToAction("Dashboard");
+                }
+                // If the creation failed, add the errors to the View Model
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpGet("dashboard")]
+        public IActionResult Dashboard()
         {
             return View();
         }
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginRegWithIdentity.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;}
+
+        [Required]
+        public string UserName {get;set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Please fulfill all password requirements.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Please fulfill all password requirements.")]
+        public string Password {get;set;}
+
+        [NotMapped]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Must match the previous entry.")]
+        public string ConfirmPassword {get;set;}
+    }
+}
